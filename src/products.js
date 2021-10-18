66ee7ecd8e7906000aa9edf42258897186301743
esmaben <felippecorrea2010@gmail.com>
2021-10-18 19:12:41 -0300

    criando arquivo de mock products.js

---
+++ src/products.js
@@ -0,0 +1,66 @@
+import img1 from './images/three-g16fb1d366_640.jpg"';
+import img2 from './images/man-g8ba6a86ce_1280.jpg';
+import img3 from './images/female-g93dbac839_1280.jpg';
+import img4 from './images/fashion-g3b223a47b_1280.jpg';
+import img5 from './images/feet-g4f33e1281_1280.jpg';
+import img6 from './images/adult-g76e97d62f_1280.jpg';
+
+export const prodCarouselOne = [
+
+    {
+        id: 1,
+        image: img1,
+        message: 'Mixed',
+        title: 'Vestidos',
+        message_preco: 'a partir de',
+        preco: 'R$599',
+    },
+
+    {
+        id: 2,
+        image: img2,
+        message: 'Dia dos namorados',
+        title: 'Presente',
+        message_preco: 'a partir de',
+        preco: 'R$69',
+    },
+    
+    {
+        id: 3,
+        image: img3,
+        message: 'Blusas',
+        title: 'Inverno 2021',
+        message_preco: 'a partir de',
+        preco: 'R$149',
+    }
+];
+
+
+export const prodCarouselTwo = [
+    {
+        id: 1,
+        image: img4,
+        message: 'Animale',
+        title: 'Animal Print',
+        message_preco: 'a partir de',
+        preco: 'R$269',
+    }, 
+
+    {
+        id: 2,
+        image: img5,
+        message: 'ALEXANDRE BRIMAN',
+        title: 'Sandálias',
+        message_preco: 'a partir de',
+        preco: 'R$349',
+    }, 
+
+    {
+        id: 3,
+        image: img6,
+        message: 'Tendência<',
+        title: 'All Jeans',
+        message_preco: 'a partir de',
+        preco: 'R$279',
+    }
+]
